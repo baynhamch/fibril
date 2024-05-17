5b4fba34097f20765d6f249aa8327d985bc73ccf
Nicholas <nicholasch24@gmail.com>
2024-05-17 18:41:40 -0400

    Initial Commit

---
+++ fibril/fibrilApp.swift
@@ -0,0 +1,17 @@
+//
+//  fibrilApp.swift
+//  fibril
+//
+//  Created by Nicholas Conant-Hiley on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct fibrilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
